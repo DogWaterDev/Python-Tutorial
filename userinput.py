b0a4ba8f603acd94038cebd2f162d73eda6ac8fd
DogWaterDev <103907925+DogWaterDev@users.noreply.github.com>
2023-06-05 14:09:02 -0500

    Delete userinput.py

--- userinput.py
+++
@@ -1,8 +0,0 @@
-name = input("What is your name? (first/last): ")
-age = int(input("What is your age? "))
-height = float(input("How tall are you in cm? "))
-#Input is always string datatype unless typecasted
-
-print("Hello " + name + ".")
-print("You are " + str(age) + " years old.")
-print("You are " + str(height) + " cm tall.")
